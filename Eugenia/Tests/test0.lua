1e842e1eda59e285cc0f0516af7c5e18c57f876e
David <labarberadavid92@gmail.com>
2018-12-02 16:31:34 +0100

    check const in prepost

---
+++ Eugenia/Tests/test0.lua
@@ -0,0 +1,12 @@
+function void main (const integer oppo)
+integer w=5
+boolean r
+boolean t=not r
+if(r and (not false))
+then
+w++
+end
+--[[{2} integer g={1,2}
+--[[pointer integer p=&g{w}
+--[[g{2}{3}{1}=g{3}{3}{1}+32
+end
